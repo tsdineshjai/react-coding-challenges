5ae626d21e41225ec0883635178e504aba8e6279
D JAI <tsdineshjai@gmail.com>
2024-06-26 11:08:28 +0530

    modal challenge completed : 1.Native-modal element 2. non-native html element

---
+++ modal-challenge/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import Modal from "./modal";
+
+function App() {
+	const [show, setShow] = React.useState(false);
+
+	const toggleModal = () => {
+		setShow((prev) => !prev);
+	};
+
+	const openModal = () => {
+		const dialogRef = document.getElementById("my-dialog");
+		dialogRef.showModal();
+	};
+	const closeModal = () => {
+		const dialogRef = document.getElementById("my-dialog");
+		dialogRef.close();
+	};
+	return (
+		<section>
+			<button onClick={toggleModal}>show the modal</button>
+
+			{show && <Modal toggleModal={toggleModal} />}
+
+			<dialog id="my-dialog">
+				<button autoFocus onClick={closeModal}>
+					Close
+				</button>
+				<p>This modal dialog has a groovy backdrop!</p>
+			</dialog>
+			<button onClick={openModal}>Show the dialog(used native dialog html element)</button>
+		</section>
+	);
+}
+
+export default App;
---
+++ modal-challenge/src/modal.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import "./modal.css";
+
+function Modal({ toggleModal }) {
+	React.useEffect(() => {
+		function CallbackFunction(e) {
+			if (e.code === "Escape") {
+				toggleModal();
+			}
+		}
+		window.addEventListener("keydown", CallbackFunction);
+		return () => {
+			window.removeEventListener("keydown", CallbackFunction);
+		};
+	}, [toggleModal]);
+	return (
+		<div className="modal-wrapper">
+			<div className="modal">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
+					explicabo voluptatibus ullam amet doloribus itaque culpa id animi
+					odit? Facilis error, quod laudantium odit delectus repellendus aliquid
+					voluptatibus porro. Facilis. Tempora debitis, perspiciatis eaque
+					necessitatibus accusantium maiores expedita qui quam iure voluptatibus
+					assumenda, possimus quos saepe aliquid dolores quo perferendis, libero
+					magni repudiandae voluptates! Consectetur ex quasi autem dolore quo.
+				</p>
+
+				<span onClick={toggleModal}>X</span>
+			</div>
+		</div>
+	);
+}
+
+export default Modal;
